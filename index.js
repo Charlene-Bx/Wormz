5f24aaebbb7d760c7232f0219886598d56e047a2
Charlene-Bx <charlene.bonnardeaux@gmail.com>
2021-07-04 23:14:28 +0200

    add button for lunch the game and two rules

--- index.js
+++ index.js
@@ -1,81 +1,218 @@
-const canva = document.querySelector("#game-zone")
-const context = canvas.getContext('2d')
+const buton = document.querySelector("#starter-buton")
+const canvas = document.querySelector("#game-zone")
+const context = canvas.getContext("2d")
 
-let params = {
-  start : 'false',
-  delay : 200,
-  grid : 20,
+// DEFINE THE STARTING PARAMETERS
+const params = {
+  start: false,
+  speed: 500,
+  grid: 20,
 }
 
+// DEFINE THE WORM AS AN OBJECT
 const worm = {
   //datas:
-  head : {
-    width : params.grid,
-    height : params.grid,
+  head: {
+    width: params.grid,
+    height: params.grid,
   },
 
   body: [],
 
-  lenght: 4,
+  length: 4,
 
-  position : {
-    x: canva.width/2,
-    y: canva.height/2,
+  position: {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
   },
 
   deplacement: {
     x: 0,
-    Y: 0,
+    y: 0,
   },
 
-  // functions:
-  display: function() {
-    context.clearRect( 0, 0, canva.width, canva.height )
-    context.fillStyle="red"
+  // ADD FUNCTION TO THE OBJECT WORM
+  display: function () {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = "#678a00"
 
-    for( let index of this.body ) {
-      context.fillRect(index.x, index.y, this.head.width - 2, this.head.height - 2)
+    for (let index of this.body) {
+      context.fillRect(
+        index.x,
+        index.y,
+        this.head.width - 2,
+        this.head.height - 2
+      )
     }
   },
 
-  move: function() {
+  move: function () {
     this.position.x += this.deplacement.x * this.head.width
     this.position.y += this.deplacement.y * this.head.height
 
     const coordinates = {
-      x : this.position.x,
-      y : this.position.y,
+      x: this.position.x,
+      y: this.position.y,
     }
 
-    this.body.push( coordinates )
+    this.body.push(coordinates)
 
-    while ( this.body.lenght > this.lenght ){
+    while (this.body.length > this.length) {
       this.body.shift()
     }
   },
 
-  grow: function() {
-    this.lenght++
-  }
+  grow: function () {
+    this.length++
+  },
 }
 
+// DEFINE THE APPLE AS AN OBJECT
 const apple = {
-  ray: grid/2,
+  ray: params.grid / 2,
 
-  position : {
-    x: ( Math.floor( Math.random() * (canvas.width/grid) ) * grid ),
-    y: ( Math.floor( Math.random() * (canvas.height/grid) ) * grid ),
+  position: {
+    x: Math.floor(Math.random() * (canvas.width / params.grid)) * params.grid,
+    y: Math.floor(Math.random() * (canvas.height / params.grid)) * params.grid,
   },
 
-  display: function() {
+  // ADD FUNCTIONS TO THIS OBJECT
+  display: function () {
     context.beginPath()
-    context.arc( this.position.x + this.ray, this.position.y + this.ray, this.ray, 0, Math.PI * 2 )
-    context.fillStyle = "green"
+    context.arc(
+      this.position.x + this.ray,
+      this.position.y + this.ray,
+      this.ray,
+      0,
+      Math.PI * 2
+    )
+    context.fillStyle = "#e6241c"
     context.fill()
     context.closePath()
-  }
+  },
 
-  initialise: function() {
-    this.position.x
+  initialise: function () {
+    this.position.x =
+      Math.floor(Math.random() * (canvas.width / params.grid)) * params.grid
+    this.position.y =
+      Math.floor(Math.random() * (canvas.height / params.grid)) * params.grid
+  },
+}
+
+const rules = {
+  if_eat_apple: () => {
+    if (
+      worm.position.x === apple.position.x &&
+      worm.position.y === apple.position.y
+    ) {
+      apple.initialise()
+      worm.grow()
+
+      params.speed >= 100 && (params.speed -= 5)
+    }
+  },
+
+  if_touch_border: () => {
+    if (
+      worm.position.x + worm.head.width === canvas.width &&
+      worm.deplacement.x === 1
+    ) {
+      return (worm.position.x = 0 - worm.head.width)
+    }
+
+    if (
+      worm.position.y + worm.head.height === canvas.height &&
+      worm.deplacement.y === 1
+    ) {
+      return (worm.position.y = 0 - worm.head.height)
+    }
+
+    if (worm.position.x === 0 && worm.deplacement.x === -1) {
+      return (worm.position.x = canvas.width)
+    }
+
+    if (worm.position.y === 0 && worm.deplacement.y === -1) {
+      return (worm.position.y = canvas.height)
+    }
+  },
+
+  if_touch_himself: () => {},
+}
+
+//CREATE THE FUNCTION FOR RUN THE GAME
+function play() {
+  //HIDE THE STARTER BUTTON
+  buton.classList.add("main__button--hide")
+  //MAKE THE WORM
+  worm.display()
+  worm.move()
+  //DISPLAY APPLE
+  apple.display()
+  //ADD RULES TO THE GAME
+  rules.if_eat_apple()
+  rules.if_touch_border()
+  rules.if_touch_himself()
+  //ANIME ALL THIS SHIT
+  setTimeout(play, params.speed)
+}
+
+//CREATE A FUNCTION FOR RESTART THE GAME AT THE INITIAL STATE
+function replay() {
+  worm.length = 4
+  worm.position.x = canvas.width / 2
+  worm.position.y = canvas.height / 2
+  worm.deplacement.x = 0
+  worm.deplacement.y = 0
+  worm.body = []
+  apple.initialise()
+  params.speed = 200
+  !params.start && game()
+}
+
+//CREATE A FUNCTION FOR CONTROLLING
+function control(keyboard) {
+  switch (keyboard.key) {
+    case "ArrowRight":
+      if (worm.deplacement.x === -1) {
+        break
+      }
+      worm.deplacement.x = 1
+      worm.deplacement.y = 0
+      break
+
+    case "ArrowLeft":
+      if (worm.deplacement.x === 1) {
+        break
+      }
+      worm.deplacement.x = -1
+      worm.deplacement.y = 0
+      break
+
+    case "ArrowUp":
+      if (worm.deplacement.y === 1) {
+        break
+      }
+      worm.deplacement.x = 0
+      worm.deplacement.y = -1
+      break
+
+    case "ArrowDown":
+      if (worm.deplacement.y === -1) {
+        break
+      }
+      worm.deplacement.x = 0
+      worm.deplacement.y = 1
+      break
+
+    case "Enter":
+      replay()
+      break
+
+    case " ":
+      worm.deplacement.x = 0
+      worm.deplacement.y = 0
+      break
   }
 }
+
+window.addEventListener("keydown", control)
